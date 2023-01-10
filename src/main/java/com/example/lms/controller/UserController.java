27f8879c17153fd6cebf0eba29453ba890ee3025
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2023-01-10 14:52:26 +0100

    added files

--- src/main/java/com/example/lms/controller/UserController.java
+++ src/main/java/com/example/lms/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.example.lms.controller;
 
 
+import com.example.lms.model.Book;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+
+    @GetMapping("/book/{isbn}")
+    public Book borrowBook(){
+
+    }
 }
